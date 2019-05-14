064e648b800c5803c7f334ad87044deb78ef0a34
Matthew Molyett <mmolyett@cisco.com>
2019-05-14 14:03:23 -0700

    GUI poc

--- first_plugin_ghidra/first.py
+++ first_plugin_ghidra/first.py
@@ -18,15 +18,33 @@ from ghidra.program.model.data import DataTypeManager
 from ghidra.util.data.DataTypeParser import AllowedDataTypes
 
  
-tool = state.getTool()
-dtm = currentProgram.getDataTypeManager()
+from javax.swing import JFrame, JLabel, JButton, WindowConstants
+from java.awt import BorderLayout
 
-print dir(currentProgram.functionManager)
-print currentProgram.functionManager
-print currentProgram.functionManager.getFunctionCount()
-print help(currentProgram.functionManager.getFunctions)
-for func in currentProgram.functionManager.getFunctions(False):
-    print func
+class Example:
+   def setText(self,event):
+       self.label.text = "{}".format(currentProgram)
+ 
+   def __init__(self):
+     frame = JFrame("Jython Example JButton")
+     frame.setSize(100, 100)
+     frame.setLayout(BorderLayout())
+     self.label = JLabel('Hello from Jython')
+     frame.add(self.label, BorderLayout.NORTH)
+     button = JButton('Click Me',actionPerformed=self.setText)
+     frame.add(button, BorderLayout.SOUTH)
+     frame.setVisible(True)
+
+frame = Example()
+# tool = state.getTool()
+# dtm = currentProgram.getDataTypeManager()
+
+# print dir(currentProgram.functionManager)
+# print currentProgram.functionManager
+# print currentProgram.functionManager.getFunctionCount()
+# print help(currentProgram.functionManager.getFunctions)
+# for func in currentProgram.functionManager.getFunctions(False):
+#     print func
 
 # selectionDialog = DataTypeSelectionDialog(tool, dtm, -1, AllowedDataTypes.FIXED_LENGTH)
 # tool.showDialog(selectionDialog)
